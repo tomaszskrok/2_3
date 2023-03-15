4d7f271637828a82e493597672cbda8a1be48fbb
tomaszskrok1 <126695915+tomaszskrok@users.noreply.github.com>
2023-03-15 15:33:58 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) 
+  {
+    int suma = 0;
+    for(int i = 0; i <= 100; i+=2)
+      {
+      suma += i;
+      }
+    System.out.println("Suma liczb parzystych od 0 do 100 wynosi " + suma);
+  }
+}
